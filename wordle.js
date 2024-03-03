88e55dddb1d7559a15cb34c1a8407ab844136eac
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2024-03-03 12:53:36 -0500

    added basic submit function to move to next line. backspace function complete.

--- wordle.js
+++ wordle.js
@@ -16,6 +16,20 @@ async function init() {
         letters[ANSWER_LENGTH * row + currentGuess.length - 1].innerText = letter;
     }
 
+    function submit() {
+        if (currentGuess.length == ANSWER_LENGTH) {
+            row++;
+            currentGuess = '';
+        }
+    };
+
+    function backspace() {
+        if (currentGuess !== '') {
+            letters[ANSWER_LENGTH * row + currentGuess.length - 1].innerText = '';
+            currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+        }
+    };
+
     document.addEventListener('keydown', function handleKeyDown (event) {
         const action = event.key;
 
