1044c74af5fa57348cb96249e33de9df5c1037a7
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2024-03-08 16:50:27 -0500

    added highlight() to show which key is pressed

--- wordle.js
+++ wordle.js
@@ -1,11 +1,38 @@
 const letters = document.querySelectorAll('.letter');
 const keys = document.querySelectorAll('.key');
+const keyValueArray = Array.from(keys).map((key) => {
+    return key.innerText.toLowerCase();
+})
+
+console.log(keyValueArray);
 
 async function init() {
 
-    document.addEventListener('keyup', function(event) {
-        console.log(event.key);
+    document.addEventListener('keyup', function handleKeyup (event) {
+        
+        if ( !isLetter(event.key)) {
+            return
+        }
+        
+        highlight(event.key);
+
     })
 };
+
+
+function isLetter(letter){
+    return /^[a-zA-Z]$/.test(letter);
+};
+
+
+function highlight(key) {
+    let keyIndex = keyValueArray.indexOf(key);
+    keys[keyIndex].classList.toggle("target");
+
+    setTimeout(() => {
+        keys[keyIndex].classList.toggle("target");
+    }, 50);
+};
+
 
 init();
