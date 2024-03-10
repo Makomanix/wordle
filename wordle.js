6fc2c79cd8f6df74ce837fd0a230e9d358a1e712
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2024-03-10 11:16:00 -0400

    added POST validation of currentGuess and markInvalid to show user incorrect guess, created getMap of fetched word

--- wordle.js
+++ wordle.js
@@ -1,24 +1,25 @@
+const ANSWER_LENGTH = 5;
+const GUESSES = 6;
 const letters = document.querySelectorAll('.letter');
 const keys = document.querySelectorAll('.key');
 const keyValueArray = Array.from(keys).map((key) => {
     return key.innerText.toLowerCase();
 });
 
-const ANSWER_LENGTH = 5;
-
-// console.log(letters[1].innerText = "K");
 
 async function init() {
     let currentGuess = '';
     let currentRow = 0;
-    let wordParts = getMap(word);
     
-
+    
     const res = await fetch ('https://words.dev-apis.com/word-of-the-day');
     const resObj = await res.json();
     const word = resObj.word.toUpperCase();
-
+    
     console.log(word);
+
+    let wordParts = getMap(word);
+    console.log(wordParts);
 
     function addLetter(letter) {
         console.log(letter)
@@ -27,11 +28,28 @@ async function init() {
         } else {
             currentGuess += letter;
         }
-
         letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
     }
 
     async function submit() {
+        if ( currentGuess.length !== ANSWER_LENGTH) {
+            return;
+        }
+
+        // validate word
+        const res = await fetch ('https://words.dev-apis.com/validate-word', {
+            method: "POST",
+            body: JSON.stringify({ word: currentGuess }),
+        });
+        const resObj = res.json();
+        const validWord = resObj.validWord;
+
+        if ( !validWord ) {
+            markInvalid();
+            // indicate to user word is invalid
+            return;
+        }
+        // compare guess to word
         // end games after 6 guesses
         currentRow += 1;
         currentGuess = '';
@@ -46,11 +64,19 @@ async function init() {
         }
     }
 
+    function markInvalid() {
+        for ( let i = 0; i < ANSWER_LENGTH; i++) {
+            letters[ANSWER_LENGTH * currentRow + i].classList.toggle('invalid');
+    
+            setTimeout(() => {
+                letters[ANSWER_LENGTH * currentRow + i].classList.toggle('invalid');
+            }, 100);
+        }
+    }
+
     document.addEventListener("keydown", function handleKeydown(event) {
         let key = event.key;
 
-        console.log(key);
-        
         if (key === 'Enter') {
             submit();
         } else if (key === 'Backspace') {
@@ -59,7 +85,6 @@ async function init() {
             addLetter(key.toUpperCase())
             highlight(key);
         }
-
     });
 };
 
@@ -78,10 +103,18 @@ function highlight(key) {
     }, 150);
 };
 
+
 function getMap(word) {
-
+    const obj = {};
+    for ( let i = 0; i < word.length; i ++) {
+        const letter = word[i];
+        if (obj[letter]) {
+            obj[letter] ++;
+        } else {
+            obj[letter] = 1;
+        }
+    }
+    return obj;
 };
-
-
 
 init();
